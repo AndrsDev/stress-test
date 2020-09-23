565195357d49c9b99f48deda6f3fe4666b78ae58
Andres Sanabria <andrs.dev@gmail.com>
2020-09-23 15:29:41 -0600

    made postgres helloworld

---
+++ main.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+
+(async () => {
+  console.log(process.env.PG_HOST);
+  await client.connect()
+  const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
+})();
