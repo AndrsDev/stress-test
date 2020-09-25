49b133089e981ca472e2b77f226405fe11ab5c69
Andres Sanabria <andrs.dev@gmail.com>
2020-09-24 19:28:29 -0600

    added experiment to read values from console with promises

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
+const readline = require("readline");
 const { Client } = require('pg');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const client = new Client({
   host: process.env.PG_HOST,
@@ -9,10 +15,27 @@ const client = new Client({
   port: process.env.PG_PORT,
 });
 
+function write(message) {
+  console.log(message);
+}
+
+function read(message) {
+  return new Promise((resolve, reject) => {
+    rl.question(`${message} \n`, (param) => {
+      resolve(param);
+    });
+  }) 
+}
 
 (async () => {
-  await client.connect();
-  const res = await client.query('SELECT NOW()');
-  console.log(res.rows[0]);
-  await client.end()
+  // await client.connect();
+  const name = await read("What is your name?");
+
+  write(`Hello ${name}`);
+
+  rl.close();
+
+  // const res = await client.query('SELECT NOW()');
+  // console.log(res.rows[0]);
+  // await client.end()
 })();
